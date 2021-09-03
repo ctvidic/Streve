0606a46d9d0a81394fa110bfda02cbfb16c009f7
ctvidic <chrisvidic.cv@gmail.com>
2021-09-03 15:37:39 -0400

    firstRoutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+    namespace :api, defaults: {format: :json} do 
+        resources :users
+        resource :session, only: [:create, :destroy, :show]
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    root "static_pages#root"
 end
