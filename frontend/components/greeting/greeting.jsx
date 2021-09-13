72f49c9fac60444f95297c606398cdc4d6faba3d
ctvidic <chrisvidic.cv@gmail.com>
2021-09-13 17:29:36 -0400

    mainPush

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,11 +4,13 @@ import { Redirect } from 'react-router'
 import { NavLink } from 'react-router-dom'
 import { signup } from '../../util/session_api_util';
 import Dropdown from 'react-bootstrap/Dropdown'
+import Plus from './plus_dropdown'
 
 class Greeting extends React.Component{
   constructor(props){
     super(props)
-    this.state = {link: window.location.href}
+    this.state = {link: window.location.href, show: false}
+ 
   }
   componentDidMount(){
     this.setState({
@@ -27,7 +29,7 @@ class Greeting extends React.Component{
   render(){
   const sessionLinks = () => (
     <div className="login-signup">
-      <Link onClick={() => this.setState({ link: "http://localhost:3000/#/login" })} to="/login">Log In</Link>
+      <Link id="signupLink" onClick={() => this.setState({ link: "http://localhost:3000/#/login" })} to="/login">Log In</Link>
     </div>
   );
 
@@ -39,10 +41,26 @@ class Greeting extends React.Component{
 
   const signUp = () => (
     <div className="login-signup">
-      <Link to="/signup" onClick={() => this.setState({ link: "http://localhost:3000/#/signup" })}>Sign Up</Link>
+      <Link to="/signup" id="loginLink" onClick={() => this.setState({ link: "http://localhost:3000/#/signup" })}>Sign Up</Link>
     </div>
   )
   
+  const showMenu = () => {
+    if(!this.state.show){
+      this.setState({show: true})
+      console.log('its true')
+      return (
+        <div id='showmenu'>Show Menufasdfasasdfsdf</div>
+      )
+    }else{
+      this.setState({ show: false})
+      return(
+        null
+      )
+    }
+
+    
+  }
   const personalGreeting = () => (
     <div className="login-signup">
     {/* <h2 className="header-name">{currentUser.username}</h2> */}
@@ -62,7 +80,7 @@ class Greeting extends React.Component{
 
         </Dropdown.Menu>
       </Dropdown> */}
-    <div id="plusDropDown"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus-circle" class="svg-inline--fa fa-plus-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm144 276c0 6.6-5.4 12-12 12h-92v92c0 6.6-5.4 12-12 12h-56c-6.6 0-12-5.4-12-12v-92h-92c-6.6 0-12-5.4-12-12v-56c0-6.6 5.4-12 12-12h92v-92c0-6.6 5.4-12 12-12h56c6.6 0 12 5.4 12 12v92h92c6.6 0 12 5.4 12 12v56z"></path></svg></div>
+      <div id="plusDropDown" onClick={() => (showMenu())}><Plus clicked={this.state.show}/></div>
     </div>
   );
   if (this.props.currentUser) {
