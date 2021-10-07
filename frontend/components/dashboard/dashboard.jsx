433c7c6440a4de9b37b2439629d214ebf4f9d58c
ctvidic <chrisvidic.cv@gmail.com>
2021-10-07 15:54:25 -0400

    linksHover

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -49,6 +49,8 @@ class Dashboard extends React.Component{
         }
     }
 
+    
+
     distance(distance){
         if (distance){
             return distance.distance
@@ -115,10 +117,10 @@ class Dashboard extends React.Component{
                     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-circle" class="svg-inline--fa fa-user-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z"></path></svg>
                 </div>
                 <div id ="profBox">
-                    <Link to={`/users/${parseInt(this.props.currentUser.id)}`}>{this.props.currentUser.username}</Link>
+                    <Link id="profBoxLink" to={`/users/${parseInt(this.props.currentUser.id)}`}>{this.props.currentUser.username}</Link>
                     <div id="latestActivity">
                         <h1>Latest Activity</h1>
-                        <div><Link to={`/workouts/${latestActivity.workout_id}`}>{latestActivity.title} - {latestActivity.date}</Link></div>
+                        <div><Link  id="latestActivityLink" to={`/workouts/${latestActivity.workout_id}`}>{latestActivity.title} - {latestActivity.date}</Link></div>
                     </div>
                     
                 </div> 
@@ -159,7 +161,7 @@ class Dashboard extends React.Component{
                             <div id='littleProf'>
                             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-circle" class="svg-inline--fa fa-user-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z"></path></svg>
                             <div>
-                            <h1 id="workoutCreator">{this.username(activity)}</h1>
+                            <Link id="linkWorkoutCreator" to={`/users/${activity.user_id}`}><h1 id="workoutCreator">{this.username(activity)}</h1></Link>
                             <h1 id="smallWorkoutcreatedAt">{activity.date}</h1>
                             </div>
                             </div>
